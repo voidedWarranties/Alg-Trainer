ba3acc723ac62e1ad305741365f778a370a70f3a
voidedWarranties <wz68.scratch@gmail.com>
2019-01-26 23:17:47 -0800

    Enabling and disabling

--- js/AlgorithmSet.js
+++ js/AlgorithmSet.js
@@ -1,19 +1,46 @@
 this.AlgorithmSet = class AlgorithmSet {
   constructor(dataObj) {
-    this.alglist = [];
-    var sets = dataObj.sets;
-    for(var i = 0; i < sets.length; i++) {
-      var set = sets[i];
-      var algs = set.algs;
-      for(var j = 0; j < algs.length; j++) {
-        var alg = algs[j];
-        this.alglist.push(new Algorithm(alg.alg, alg.arrows));
-      }
-    }
+    this.disabledSets = [];
+    this.disabledAlgs = [];
+    this.sets = dataObj.sets;
   }
 
   getAlg() {
-    var alg = this.alglist[Math.floor(Math.random()*this.alglist.length)];
-    return alg;
+    var alglist = [];
+    for(var i = 0; i < this.sets.length; i++) {
+      var set = this.sets[i];
+      if(this.disabledSets.includes(set.name)) {
+        continue;
+      }
+      var algs = set.algs;
+      for(var j = 0; j < algs.length; j++) {
+        var alg = algs[j];
+        if(this.disabledAlgs.includes(alg.name)) {
+          continue;
+        }
+        alglist.push(new Algorithm(alg.alg, alg.arrows));
+      }
+    }
+    return alglist[Math.floor(Math.random()*alglist.length)];
+  }
+
+  setAlgSetEnabled(setName, enabled) {
+    if(!enabled) {
+      if(!this.disabledSets.includes(setName)) {
+        this.disabledSets.push(setName);
+      }
+    } else {
+      this.disabledSets.splice(this.disabledSets.indexOf(setName), enabled);
+    }
+  }
+
+  setAlgEnabled(algName, enabled) {
+    if(!enabled) {
+      if(!this.disabledAlgs.includes(algName)) {
+        this.disabledAlgs.push(algName);
+      }
+    } else {
+      this.disabledAlgs.splice(this.disabledAlgs.indexOf(algName), enabled);
+    }
   }
 }
--- js/index.js
+++ js/index.js
@@ -1,8 +1,12 @@
 Cube.initSolver();
 
 var listener = new window.keypress.Listener();
+var started = false;
 listener.simple_combo("enter", function() {
-  render();
+  if(!started) {
+    render();
+    started = true;
+  }
 });
 
 function render() {
@@ -14,6 +18,25 @@ function render() {
     $("#result").html(alg.getScramble() + "<img src=\"" + alg.getImageTD() + "\">" + "<img src=\"" + alg.getImageTransparent() + "\">");
     listener.simple_combo("backspace", function() {
       $("#solutions").html(alg.getAlg());
+    });
+    listener.simple_combo("space", function() {
+      $("#solutions").html("");
+      alg = algset.getAlg();
+      if(alg != null) {
+        $("#result").html(alg.getScramble() + "<img src=\"" + alg.getImageTD() + "\">" + "<img src=\"" + alg.getImageTransparent() + "\">");
+      }
+    });
+    listener.simple_combo("up", function() {
+      algset.setAlgSetEnabled(prompt("Disable Set"), false);
+    });
+    listener.simple_combo("down", function() {
+      algset.setAlgSetEnabled(prompt("Enable Set"), true);
+    });
+    listener.simple_combo("left", function() {
+      algset.setAlgEnabled(prompt("Disable Alg"), false);
+    });
+    listener.simple_combo("right", function() {
+      algset.setAlgEnabled(prompt("Enable Alg"), true);
     });
   }
   fr.readAsText(document.getElementById("file-input").files[0]);
